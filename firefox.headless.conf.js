752f637566c76cbc60a84966150e6f2b5b887d88
TatianaSim <s_tvl@mail.ru>
2021-05-04 19:14:30 +0300

    add commit firefox.headless.conf.js

--- firefox.headless.conf.js
+++ firefox.headless.conf.js
@@ -4,18 +4,18 @@ const base = require('./config/base.conf');
 exports.config = {
     runner: 'local',
 
-   maxInstances: 1,
+    maxInstances: 1,
     capabilities: [
         {
             maxInstances: 1,
             browserName: 'firefox',
             'moz:firefoxOptions': {
-                args: ['--window-size=1280,720']
-            }
-        },
+                args: ['--window-size=1280,720, --headless, --disable-gpu']
+            },
+        }
     ],
 
-    services: ['geckodriver'],
+   services: ['geckodriver'],
 
     ...base,
     ...hooks
